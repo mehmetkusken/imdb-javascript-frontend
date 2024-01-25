146440de6a72ad098dab7b920e469e196329e91c
Mehmet <mehmetkusken@gmail.com>
2024-01-25 13:43:49 +0300

    movie index and show page is ready

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,39 +7,6 @@ class Movie {
         this.constructor.all.push(this)  
     }
 
-    static renderIndex = () => {
-        const movieContainer = document.createElement("div")
-        movieContainer.classList.add("container")
-        const main = document.getElementById("main")
-        main.innerHTML = ""
-        main.appendChild(movieContainer)
-        this.all.forEach(movie => movie.renderCard())  
-        movieContainer.addEventListener("click" , this.clickIndex)
-
-    
-    }
-
-    static clickIndex = (e) => {
-        if(e.target.tagName == "IMG" || e.target.classList.contains("title")){
-            const id = e.target.closest(".movie-card").dataset.id
-            this.find(id).renderShow()
-        }
-    }
-    renderShow = () => {
-        const {id,title , poster , year , genre , rating , plot } = this.data
-        document.getElementById("main").innerHTML = `
-        <div class="center">
-        <p><img src= ${poster}/></p>
-        <p><strong>Title: </strong>${title}</p>
-        <p><strong>Year: </strong>${year}</p>
-        <p><strong>Genre: </strong>${genre}</p>
-        <p><strong>Plot: </strong>${plot}</p>
-        <p><strong>IMDB Rating: </strong>${rating}</p>
-       <button id="goBack">Go Back</button>
-        </div>
-        `
-        document.getElementById("goBack").addEventListener("click", Movie.renderIndex)
-    }
     renderCard = () => {
         const {id,title , poster , year , genre , rating , plot } = this.data
         document.querySelector(".container").innerHTML += `
@@ -54,10 +21,81 @@ class Movie {
         `
     }
 
-    static find = (id) => this.all.find(movie => movie.data.id == id)
+    static renderIndex = () => {
+        const movieContainer = document.createElement("div")
+        movieContainer.classList.add("container")
+        const main = document.getElementById("main")
+        main.innerHTML = ""
+        main.appendChild(movieContainer)
+        this.all.forEach(movie => movie.renderCard())  
+        movieContainer.addEventListener("click" , this.clickIndex)
+       
+    }
 
+    static clickIndex = (e) => {
+        if(e.target.tagName == "IMG" || e.target.classList.contains("title")){
+            const id = e.target.closest(".movie-card").dataset.id
+            this.find(id).renderShow()
+        }
+    }
+
+    renderShow = () => {
+        const {id,title , poster , year , genre , rating , plot } = this.data
+       
+        document.getElementById("main").innerHTML = `
+        <div class="center" id="find">
+        <p><img src= ${poster}/></p>
+        <p><strong>Title: </strong>${title}</p>
+        <p><strong>Year: </strong>${year}</p>
+        <p><strong>Genre: </strong>${genre}</p>
+        <p><strong>Plot: </strong>${plot}</p>
+        <p><strong>IMDB Rating: </strong>${rating}</p>
+        <button id="goBack">Go Back</button>   
+        <button data-id=${id}, id="addFavorite">Add to your Favorite</button>       
+        </div>
+        `
+        document.getElementById("goBack").addEventListener("click", Movie.renderIndex) 
+        document.getElementById("addFavorite").addEventListener("click", modal.open) 
+        Movie.favoriteForm(id)
+    
+    }
+
+    
+
+
+    static favoriteForm = (id) => {
+        modal.main.innerHTML = `
+        <form>
+        <p>Do you want to add your favorite list this movie?</p>
+        <button class="delete-bttn btn btn-sm btn-primary" data-movieid=${id}>Add</button>
+        
+        </form>
+        `
+        
+        
+        modal.main.querySelector('form').addEventListener('click', this.handleSubmit)
+    }
+
+    static handleSubmit = (e) => {
+        
+        if(e.target.innerText === 'Add'){
+            console.log(e.target.dataset.movieid)
+        }
+       
+
+        e.preventDefault()        
+        const addFavoriteList = {
+           
+        
+           
+
+        }
+
+    }
+
+    
    
-   
+    static find = (id) => this.all.find(movie => movie.data.id == id)
 
     static getMovies = () => {
         api.getMovies().then(movies => {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 const api = new ApiService("http://localhost:3000")
+const modal = new Modal()
 
 Movie.getMovies()
